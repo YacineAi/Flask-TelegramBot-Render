ef52869a15b6c7cac6a8224c6a97812c92eec838
Yacine Djenidi <djenidi.dev@gmail.com>
2024-01-03 19:07:14 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,22 @@
 from flask import Flask, request
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler
+from telegram import Update
+from telegram.ext import Dispatcher, CommandHandler, ContextTypes
 import os
-# Replace with your Telegram bot token
+
+app = Flask(__name__)
+dispatcher = Dispatcher(app, None, workers=0)
 TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 
-# Create Flask app
-app = Flask(__name__)
-
-# Create bot
-updater = Updater(token=TOKEN)
-dispatcher = updater.dispatcher
-
-# Define a command handler for '/start'
-def start(update, context):
-    chat_id = update.message.chat_id
-    context.bot.send_message(chat_id, "Hello World!", reply_markup=ForceReply())
-
-# Add the command handler to the dispatcher
-dispatcher.add_handler(CommandHandler("start", start))
-
-# Endpoint for the Telegram API to send updates to
 @app.route(f'/{TOKEN}', methods=['POST'])
-def webhook():
-    update = Update.de_json(request.get_json(force=True), bot)
-    updater.dispatcher.process_update(update)
+def respond():
+    update = Update.de_json(request.json, dispatcher.bot)
+    dispatcher.process_update(update)
     return 'OK'
 
-# Start Flask app
-if __name__ == "__main__":
-    app.run(debug=True)
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+dispatcher.add_handler(CommandHandler("hello", hello))
+
+if __name__ == '__main__':
+    app.run(port=5000)
