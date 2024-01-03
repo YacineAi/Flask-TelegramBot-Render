dbb93f04c4afbf017b7aff9891267cbb280c0d2e
Yacine Djenidi <djenidi.dev@gmail.com>
2024-01-03 19:00:34 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 from flask import Flask, request
 from telegram import Update, ForceReply
-from telegram.ext import Dispatcher, CommandHandler
-from telegram import Bot
+from telegram.ext import Updater, CommandHandler
 
 # Replace with your Telegram bot token
 TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
@@ -10,13 +9,13 @@ TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 app = Flask(__name__)
 
 # Create bot
-bot = Bot(token=TOKEN)
-dispatcher = Dispatcher(bot, None, workers=0)
+updater = Updater(token=TOKEN)
+dispatcher = updater.dispatcher
 
 # Define a command handler for '/start'
 def start(update, context):
     chat_id = update.message.chat_id
-    bot.send_message(chat_id, "Hello World!", reply_markup=ForceReply())
+    context.bot.send_message(chat_id, "Hello World!", reply_markup=ForceReply())
 
 # Add the command handler to the dispatcher
 dispatcher.add_handler(CommandHandler("start", start))
@@ -25,7 +24,7 @@ dispatcher.add_handler(CommandHandler("start", start))
 @app.route(f'/{TOKEN}', methods=['POST'])
 def webhook():
     update = Update.de_json(request.get_json(force=True), bot)
-    dispatcher.process_update(update)
+    updater.dispatcher.process_update(update)
     return 'OK'
 
 # Start Flask app
